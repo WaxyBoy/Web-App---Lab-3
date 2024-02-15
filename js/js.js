b5246798abf31277c898102abb3dcead8793031c
WaxyBoy <144962934+WaxyBoy@users.noreply.github.com>
2024-02-15 10:12:32 -0600

    Add files via upload

--- js/js.js
+++ js/js.js
@@ -1,19 +1,27 @@
 function popup(x)
 
 {
-
 	document.getElementById('popup'+x).style.display='block';
-
 	document.getElementById('fade'+x).style.display='block';
-
 }
 
 function popupclose(x)
 {
-
 	document.getElementById('popup'+x).style.display='none';
-
 	document.getElementById('fade'+x).style.display='none';	
-
 }
 
+var newitem = ''
+	function addcart(x)
+	{
+		document.getElementById('input').innerHTML += x;
+		newitem += x;
+	}
+
+	function displaycart()
+	{
+		document.getElementById('output').innerHTML +="<br>" + newitem;
+		newitem = '';
+		document.getElementById('input').innerHTML = '';
+		document.getElementById('cart').style.display='block';
+	}
